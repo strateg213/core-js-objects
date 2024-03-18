a62dfac03b4e5bd9e9f4ebb4ad442c3257a800a2
strateg213 <strateg213@gmail.com>
2024-03-18 23:05:01 +0300

    feat: update task

--- src/objects-tasks.js
+++ src/objects-tasks.js
@@ -17,8 +17,8 @@
  *    shallowCopy({a: 2, b: { a: [1, 2, 3]}}) => {a: 2, b: { a: [1, 2, 3]}}
  *    shallowCopy({}) => {}
  */
-function shallowCopy(/* obj */) {
-  throw new Error('Not implemented');
+function shallowCopy(obj) {
+  return Object.assign(obj);
 }
 
 /**
@@ -32,8 +32,8 @@ function shallowCopy(/* obj */) {
  *    mergeObjects([{a: 1, b: 2}, {b: 3, c: 5}]) => {a: 1, b: 5, c: 5}
  *    mergeObjects([]) => {}
  */
-function mergeObjects(/* objects */) {
-  throw new Error('Not implemented');
+function mergeObjects(objects) {
+  return Object.entries(objects);
 }
 
 /**
@@ -199,8 +199,7 @@ function fromJSON(/* proto, json */) {
  *      { country: 'Belarus', city: 'Brest' }
  *    ]
  *                      =>
- *    [
- *      { country: 'Belarus', city: 'Brest' },
+ *    [https://github.com/strateg213/core-js-objects.git
  *      { country: 'Belarus', city: 'Minsk' },
  *      { country: 'Poland',  city: 'Krakow' },
  *      { country: 'Poland',  city: 'Warsaw' },
